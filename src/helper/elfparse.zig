fbf877a7a880954551e71b03cef64861ba217155
hrasity <hrasity@gmail.com>
2026-05-19 10:42:32 +0300

    parse part going generic

--- src/helper/elfparse.zig
+++ src/helper/elfparse.zig
@@ -15,24 +15,25 @@ pub fn parseElf(data: []const u8) !void {
     if (class == ELF.ELFCLASS32) {
         // TODO: 32bit parse...
         std.log.info("class 1 32 bit", .{});
-        try parseElf32(data);
+        try elfParser(ELF.Elf32Header, data);
     } else if (class == ELF.ELFCLASS64) {
         // TODO: 64bit parse...
         std.log.info("class 2 64bit", .{});
+        try elfParser(ELF.ElF64Header, data);
     } else {
         e("[ERROR] Unknown ELF class {d}", .{class});
     }
 }
 
-pub fn parseElf32(data: []const u8) !void {
+pub fn elfParser(comptime header: type, data: []const u8) !void {
     if (data.len < @sizeOf(ELF.Elf32Header)) {
         e("[ERROR] so small for ELF32\n", .{});
         return;
     }
 
-    const hdr = std.mem.bytesAsValue(ELF.Elf32Header, data[0..@sizeOf(ELF.Elf32Header)]);
+    const hdr = std.mem.bytesAsValue(header, data[0..@sizeOf(header)]);
     print("\n", .{});
-    print("{s:<12}: ELF32 (32bit)\n", .{"CLASS"});
+    print("{s:<12}: {s}\n", .{ "CLASS", if (header == ELF.Elf32Header) "ELF32" else "ELF64" });
     print("{s:<12}: {s}\n", .{ "DATA", if (data[5] == 1) "LittleEndian" else "BigEndian" });
     print("{s:<12}: {s}\n", .{ "TYPE", elfHelper.elfTypeName(hdr.e_type) });
     print("{s:<12}: .{s}\n", .{ "MACHINE", elfHelper.elfMachineName(hdr.e_machine) });
