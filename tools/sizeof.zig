b843ae69e1f405031631ce99e7fc25fbea6e0c9b
hrasity <hrasity@gmail.com>
2026-05-22 01:11:59 +0300

    some addings will continue

--- tools/sizeof.zig
+++ tools/sizeof.zig
@@ -7,9 +7,12 @@ pub fn main() void {
     print(
         \\Size of ELF32Header {d}
         \\Size of ELF64Header {d}
-        \\
+        \\Size of Elf32 Program Header {d} 
+        \\Size of Elf32SectionHeader {d}
     , .{
         @sizeOf(structs.Elf32Header),
         @sizeOf(structs.Elf64Header),
+        @sizeOf(structs.Elf32ProgramHeader),
+        @sizeOf(structs.Elf32SectionHeader),
     });
 }
