636339a4cfced7908e7a1d1696fa8b8053f212e7
hrasity <hrasity@gmail.com>
2026-05-21 15:57:35 +0300

    Added program header part..

--- src/helper/elfparse.zig
+++ src/helper/elfparse.zig
@@ -14,11 +14,11 @@ pub fn parseElf(data: []const u8) !void {
     const class = data[4];
     if (class == ELF.ELFCLASS32) {
         // TODO: 32bit parse...
-        std.log.info("class 1 32 bit", .{});
+        // std.log.info("class 1 32 bit", .{});
         try elfParser(ELF.Elf32Header, ELF.Elf32ProgramHeader, ELF.Elf32SectionHeader, data);
     } else if (class == ELF.ELFCLASS64) {
         // TODO: 64bit parse...
-        std.log.info("class 2 64bit", .{});
+        // std.log.info("class 2 64bit", .{});
         try elfParser(ELF.Elf64Header, ELF.Elf64ProgramHeader, ELF.Elf64SectionHeader, data);
     } else {
         e("[ERROR] Unknown ELF class {d}", .{class});
@@ -26,7 +26,6 @@ pub fn parseElf(data: []const u8) !void {
 }
 
 pub fn elfParser(comptime header: type, comptime programHeader: type, comptime sectionHeader: type, data: []const u8) !void {
-    _ = sectionHeader;
     if (data.len < @sizeOf(ELF.Elf32Header)) {
         e("[ERROR] so small for ELF32\n", .{});
         return;
@@ -44,22 +43,21 @@ pub fn elfParser(comptime header: type, comptime programHeader: type, comptime s
     print("{s:<12}: 0x{X}\n", .{ "FLAGS", hdr.e_flags });
 
     // Program Header
-    print("\n\n  {s:<10} {s:<5} {s:<18} {s:<18} {s:<14} {s:<14}\n", .{ "Type", "Flg", "VAddr", "PAddr", "FileSize", "MemSize" });
-    print("  {s}\n", .{"-" ** 78});
+    print("\nPROGRAM HEADER", .{});
+    print("\n\n  {s:<14} {s:<4} {s:<19} {s:<19} {s:<13} {s:<14}\n", .{ "Type", "Flg", "VAddr", "PAddr", "FileSize", "MemSize" });
+    print("  {s}\n", .{"-" ** 86});
 
     for (0..hdr.e_phnum) |index| {
         //print("{d} {X}\n", .{ index, @as(usize, hdr.e_phoff) + @as(usize, index) * hdr.e_phentsize });
-        const off = @as(usize, hdr.e_phoff) + @as(usize, index) * hdr.e_phentsize;
-        if (off + @sizeOf(programHeader) > data.len) break;
-        //print("{X}\n", .{of});
-        const ph = std.mem.bytesAsValue(programHeader, data[off..][0..@sizeOf(programHeader)]);
+        const p_off = @as(usize, hdr.*.e_phoff) + @as(usize, index) * hdr.e_phentsize;
+        if (p_off + @sizeOf(programHeader) > data.len) break;
+        //print("{X}\n", .{off});
+        const ph = std.mem.bytesAsValue(programHeader, data[p_off..][0..@sizeOf(programHeader)]);
         const flags = elfHelper.segmentFlags(ph.p_flags);
         //print("{s}\n", .{flags});
-        print("  {s:<10} {c}{c}{c}  0x{X:0>16}  0x{X:0>16}  0x{X:0>10}  0x{X:0>10}\n", .{
+        print("  {s:<14} {s}  0x{X:0>16}  0x{X:0>16}  0x{X:0>10}  0x{X:0>10}\n", .{
             elfHelper.segmentTypeName(ph.p_type),
-            flags[0],
-            flags[1],
-            flags[2],
+            flags, // what ı thougt...
             ph.p_vaddr,
             ph.p_paddr,
             ph.p_filesz,
@@ -67,4 +65,31 @@ pub fn elfParser(comptime header: type, comptime programHeader: type, comptime s
         });
     }
     print("\n", .{});
+
+    print("SECTION HEADER", .{});
+    print("\n\n  {s:<4} {s:<20} {s:<12} {s:<18} {s:<12} {s:<12} {s:<8}\n", .{
+        "Nr", "Name", "Type", "Address", "Offset", "Size", "Align",
+    });
+    print("  {s}\n", .{"-" ** 94});
+
+    for (0..hdr.e_shnum) |index| {
+        //print("{d} {X}\n", .{ index, @as(usize, hdr.e_phoff) + @as(usize, index) * hdr.e_shentsize });
+        const s_off = @as(usize, hdr.e_shoff) + @as(usize, index) * hdr.e_shentsize;
+        if (s_off + @sizeOf(sectionHeader) > data.len) break;
+        const sh = std.mem.bytesAsValue(sectionHeader, data[s_off..][0..@sizeOf(sectionHeader)]);
+
+        const name = "---";
+        print(
+            "  {d:<4} {s:<20} {s:<12} 0x{X:0>16}  0x{X:0>8}  0x{X:0>8}   {d:<8}\n",
+            .{
+                index,
+                name,
+                elfHelper.sectionType(sh.sh_type),
+                sh.sh_addr,
+                sh.sh_offset,
+                sh.sh_size,
+                sh.sh_addralign,
+            },
+        );
+    }
 }
