4389972c903fe220550e655f15813957530ecf60
hrasity <hrasity@gmail.com>
2026-05-29 05:39:59 +0300

    change for init.minimum args issue

--- src/main.zig
+++ src/main.zig
@@ -2,63 +2,127 @@ const std = @import("std");
 const elf = @import("helper/elf.zig");
 const win = @import("helper/win.zig");
 const elfparser = @import("helper/elfparse.zig");
+const winparser = @import("helper/winparse.zig");
+const helper = @import("helper/mainhelper.zig");
 
 const e = std.log.err;
 const info = std.log.info;
+
+fn usage() void {
+    std.debug.print(
+        \\biber v0.1
+        \\
+        \\Usage:
+        \\  biber -f <file> [options]
+        \\
+        \\Common:
+        \\  -headers
+        \\  -sections
+        \\  -symbols
+        \\  -all
+        \\
+        \\PE only:
+        \\  -pe-directories
+        \\  -pe-imports
+        \\  -pe-exports
+        \\  -pe-debug
+        \\  -pe-exceptions
+        \\
+        \\ELF only:
+        \\  -elf-programs
+        \\  -elf-dynamic
+        \\  -elf-relocs
+        \\  -elf-notes
+        \\
+        \\Raw:
+        \\  -dump <offset> <length>
+        \\  -dis  <offset> <length>
+        \\
+        \\Examples:
+        \\  biber -f limon.exe -headers
+        \\  biber -f limon.exe -sections
+        \\  biber -f limon.exe -pe-imports
+        \\  biber -f kernel -elf-programs
+        \\  biber -f zig.exe -all
+        \\  biber -f app.exe -dump 0x400 128
+        \\
+    , .{});
+}
+
+fn hasNoAction(opt: helper.Options) bool {
+    return !opt.all and
+        !opt.headers and
+        !opt.sections and
+        !opt.symbols and
+        !opt.pe_directories and
+        !opt.pe_imports and
+        !opt.pe_exports and
+        !opt.pe_debug and
+        !opt.pe_exceptions and
+        !opt.elf_programs and
+        !opt.elf_dynamic and
+        !opt.elf_relocs and
+        !opt.elf_notes and
+        !opt.dump and
+        !opt.dis;
+}
 
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
     const io = init.io;
 
-    var start_point: [:0]const u8 = undefined;
-    var end_point: [:0]const u8 = undefined;
-    var exe_name: [:0]const u8 = undefined;
+    //var start_point: [:0]const u8 = undefined;
+    //var end_point: [:0]const u8 = undefined;
+    //var exe_name: [:0]const u8 = undefined;
 
     const args = try init.minimal.args.toSlice(gpa);
+    defer gpa.free(args); // !!! dont forget ...
     //std.debug.print("-- args len {d} --", .{args.len});
     //if (args.len != 4) {
     //    std.debug.print("biber v0.1\nbiber [app.exe] [start_point] [end_point]\n", .{});
     //    return;
     //}
+
+    //if (args.len < 2) {
+    //    std.debug.print(
+    //        \\biber v0.1
+    //        \\biber [filename]
+    //        \\biber [filename] [start_point] [end_point]
+    //    , .{});
+    //}
+
     if (args.len < 2) {
-        std.debug.print(
-            \\biber v0.1
-            \\biber [filename]
-            \\biber [filename] [start_point] [end_point]            
-        , .{});
+        usage();
+        return;
     }
-    exe_name = args[1];
-    start_point = args[2];
-    end_point = args[3];
+
+    var opt = helper.parseArgs(args) catch |err| {
+        std.debug.print("Error: {}\n", .{err});
+        usage();
+        return;
+    };
+
+    if (opt.file == null) {
+        usage();
+        return;
+    }
+
+    // Sadece `biber -f file.exe` yazılırsa default olarak header bas.
+    if (hasNoAction(opt)) {
+        opt.headers = true;
+    }
+    //exe_name = args[1];
+    //start_point = args[2];
+    //end_point = args[3];
 
     //std.log.info("{s} {s} {s}", .{ exe_name, start_point, end_point });
     //const offset = try std.fmt.parseInt(usize, start_point, 10);
     //const length = try std.fmt.parseInt(usize, end_point, 10);
-    const file = try std.Io.Dir.cwd().openFile(io, exe_name, .{ .mode = .read_only });
+    const file = try std.Io.Dir.cwd().openFile(io, opt.file.?, .{ .mode = .read_only });
     defer file.close(io);
 
     // Diassembler part
-    if (args.len == 4) {
-        const offset = try std.fmt.parseInt(usize, start_point, 10);
-        const length = try std.fmt.parseInt(usize, end_point, 10);
-
-        var read_buffer: [1024]u8 = undefined;
-        var reader = file.reader(io, &read_buffer);
-
-        try reader.seekTo(offset);
-        const bytes = try reader.interface.readAlloc(gpa, @as(usize, length));
-        defer gpa.free(bytes);
-        //std.log.info("{s}", .{bytes});
-        var i: usize = 0;
-        std.debug.print("\n", .{});
-        while (i < bytes.len) : (i += 16) {
-            std.debug.print("{X:0>8}  ", .{offset + i});
-            const row_end = @min(i + 16, bytes.len);
-            for (bytes[i..row_end]) |b| std.debug.print("{X:0>2} ", .{b});
-            std.debug.print("\n", .{});
-        }
-        std.debug.print("\n", .{});
-    }
+    //if (args.len == 4) {}
 
     var read_buffer: [4096]u8 = undefined;
     var reader = file.reader(io, &read_buffer);
@@ -72,18 +136,38 @@ pub fn main(init: std.process.Init) !void {
     try reader.interface.appendRemainingUnlimited(gpa, &list);
 
     const data = list.items;
-
-    // TODO: MACH-O eklemek lazım
-    if (data.len >= 4 and std.mem.eql(u8, data[0..4], &elf.ELFMAG)) {
-        //info("content -> {s}", .{data[0..4]});
-        //info("linux file", .{});
-        try elfparser.parseElf(data);
-    } else if (data.len >= 2 and std.mem.readInt(u16, data[0..2], .little) == win.DOS_MAGIC) {
-        //TODO: winfile support
-        info("content -> {s}", .{data[0..2]});
-        info("win file", .{});
-    } else {
-        e("[ERROR] Not supported", .{});
+    if (opt.dump) {
+        helper.hexDump(data, opt.offset, opt.length);
         return;
     }
+
+    if (opt.dis) {
+        std.debug.print("DISASSEMBLER VIEW\n", .{});
+        helper.hexDump(data, opt.offset, opt.length);
+        return;
+    }
+
+    switch (helper.detectFormat(data)) {
+        .elf => try helper.runElf(data, opt),
+        .pe => try helper.runPe(data, opt),
+        .unknown => {
+            e("[ERROR] Not supported", .{});
+            return;
+        },
+    } //std.debug.print("args.len: {d}\n", .{args.len});
+    //std.debug.print("file magic: {x} {x} {x} {x}\n", .{ data[0], data[1], data[2], data[3] });
+    // TODO: MACH-O eklemek lazım
+    //if (data.len >= 4 and std.mem.eql(u8, data[0..4], &elf.ELFMAG)) {
+    //info("content -> {s}", .{data[0..4]});
+    //info("linux file", .{});
+    //    try elfparser.parseElf(data);
+    //} else if (data.len >= 2 and std.mem.readInt(u16, data[0..2], .little) == win.DOS_MAGIC) {
+    //TODO: winfile support
+    //info("content -> {s}", .{data[0..2]});
+    //info("win file", .{});
+    //try winparser.parsePe(data);
+    //} else {
+    //    e("[ERROR] Not supported", .{});
+    //    return;
+    //}:w
 }
